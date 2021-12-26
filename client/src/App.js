45b39fe75d495876aa6b4f08d65f17e790d795fb
John Chaffee <johnchaffee@mac.com>
2021-12-26 10:42:09 -0800

    Remove unneeded files: reportWebVitals.js, App.tst.js, setupTests.js, logo.svg

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
-import logo from "./logo.svg"
 import "./App.css"
 import Messages from "./Messages"
 
--- client/src/Messages.js
+++ client/src/Messages.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react"
-import "./messages.css"
+import React from "react"
 
 export default function Messages({ messages }) {
   return (
