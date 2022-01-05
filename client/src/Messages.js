1d2c6d7f87ce56083bf951efb56cd00e3539ebb5
John Chaffee <johnchaffee@mac.com>
2022-01-04 18:34:08 -0800

    useRef → Scroll into view

--- client/src/Messages.js
+++ client/src/Messages.js
@@ -1,11 +1,24 @@
-import React from "react"
+import React, { useRef, useEffect } from "react"
 
 export default function Messages({ messagesList }) {
   console.log("MESSAGES LIST: ", messagesList)
+
+  const divRef = useRef()
+
+  useEffect(() => {
+    if (divRef.current) {
+      divRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest",
+      })
+    }
+  })
+
   return (
     <div className="content">
       {messagesList.map((message) => (
-        <div key={message.id} className={message.direction}>
+        <div key={message.id} className={message.direction} ref={divRef}>
           <div className="message-bubble">{message.body}</div>
           <div className="message-footer">Twilio • Time</div>
         </div>
